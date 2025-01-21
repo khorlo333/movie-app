5c58057c9e8e5f3ddcc7c3bdcf435716986dea91
24LP3394 24LP3394 <24LP3394@G304-CC0234.local>
2025-01-21 12:59:29 +0800

    components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,49 @@
-"use client";
+import Image from "next/image";
 
-import { useEffect, useState } from "react";
-
-type MovieType = {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: [];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-};
-
-export default function Home() {
-  const [movies, setMovies] = useState<MovieType[] | undefined>();
-  // const [card, setCard] = useState<MovieType | undefined>();
-
+export default async function Home() {
   const token =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjZjOTk3YzEzOGMyZGQ3YjMwN2NkMDA3NzlmNTc3MyIsIm5iZiI6MTczNzM0MzExNS43NTYsInN1YiI6IjY3OGRjMDhiMWFmYzM0NjQ2NzY1MzI2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qBQwPUQI1eZolIH5EZEEVC4fL-9xbUbAEHT89iXp7W0";
-  const getMovie = async () => {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    if (data?.results) {
-      setMovies(data.results);
+
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
     }
-  };
-  // const getCard = async () => {
-  //   const response = await fetch(
-  //     // "https://api.themoviedb.org/3/movie/{539972}/images",
-  //     `https://image.tmdb.org/t/p=`,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //     }
-  //   );
-  //   const data = await response.json();
-  //   if (data?.results) {
-  //     setCard(data.results[0]);
-  //   }
-  // };
-  useEffect(() => {
-    getMovie();
-    // getCard();
-    console.log("getting movie");
-  }, []);
-  console.log({ movies });
+  );
+  const data = await response.json();
+
   return (
-    <div className="flex justify-center items-center w-[100vw] h-[100vh] text-green-500 text-[20px] relative">
-      {movies?.map((movie, index) => {
+    <div className="flex justify-center items-center max-w-[1440px] h-[100vh] px-[80px] flex-wrap gap-5  ">
+      {data.results?.slice(0, 10).map((movie: MovieType) => {
         return (
           <div
-            className="flex justify-between w-[1280px] max-h-[980px] object-contain flex-wrap absolute"
-            key={index}
+            key={movie.id}
+            className=" w-[230px] object-contain bg-[#f4f4f5]"
           >
             <div className="max-w-[230px] h-[440px] rounded-2 ">
               <img
-                className="w-[100%] h-[100%] object-cover "
+                className="w-[100%] h-[340px] object-cover rounded-2  "
                 src={"https://image.tmdb.org/t/p/original/" + movie.poster_path}
                 alt=""
               />
             </div>
-            <div>{movie.original_title}</div>
+            <p className="text-[12px] flex ">
+              <span className="text-[14px] font-bold flex">
+                <Image
+                  alt={movie.original_title}
+                  width={1000}
+                  height={1000}
+                  src={"star.svg"}
+                  className="w-4 h-4"
+                />
+                {movie.vote_average.toFixed(1)}
+              </span>
+              /10
+            </p>
+            <h3 className="text-[18px]">{movie.original_title}</h3>
           </div>
         );
       })}
