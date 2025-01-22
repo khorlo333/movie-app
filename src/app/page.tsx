b4a2a3b14478a6fd149f71492758227dfc941b40
24LP3394 24LP3394 <24LP3394@G304-CC0234.local>
2025-01-22 12:46:50 +0800

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
 
 import Card from "./_components/Card";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const token =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjZjOTk3YzEzOGMyZGQ3YjMwN2NkMDA3NzlmNTc3MyIsIm5iZiI6MTczNzM0MzExNS43NTYsInN1YiI6IjY3OGRjMDhiMWFmYzM0NjQ2NzY1MzI2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qBQwPUQI1eZolIH5EZEEVC4fL-9xbUbAEHT89iXp7W0";
 
+  const upcomingResponse = await fetch(
+    "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
   const response = await fetch(
     "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
     {
@@ -24,12 +34,20 @@ export default async function Home() {
       },
     }
   );
+  const upcomingData = await upcomingResponse.json();
   const data = await response.json();
   const topRatedData = await topRatedResponse.json();
 
   return (
-    <div className="flex flex-col justify-center items-center mt-5">
+    <div className="flex flex-col justify-center items-center my-5">
+      <div className="w-[1400px] px-[80px] flex justify-between items-center">
+        <h3>Upcoming</h3>
+        <Button>See more</Button>
+      </div>
+      <Card data={upcomingData.results} />
+      <h3>Popular</h3>
       <Card data={data.results} />
+      <h3>Top rated</h3>
       <Card data={topRatedData.results} />
     </div>
   );
